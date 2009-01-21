358324cdfee0ef511456f30146be441883715d9f
Greg Borenstein <greg@mfdz.com>
2009-01-22 04:06:42 +0800

    wrap request in a timeout
    Signed-off-by: stepchange <mjfreshyfresh+scg@gmail.com>

--- freddy.rb
+++ freddy.rb
@@ -1,7 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'open-uri'
+require 'timeout'
 
 get '/' do
-  "#{params['callback']}(#{open(params['js']).read})"
+  begin
+    Timeout::timeout(15) do 
+      "#{params['callback']}(#{open(params['url']).read})"
+    end
+  rescue Timeout::Error
+    "#{params['callback']}({'error' : 'Requesting the json took too long. Time limit is 15 seconds.'})"
+  end
 end
